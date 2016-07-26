e602ddacf39fdb321d2830e48a01f41d946d65bd
Romain BEAL <r.beal@laposte.net>
2016-07-26 23:17:01 +0200

    correct net

--- main.lua
+++ main.lua
@@ -12,12 +12,15 @@ local event = require("event")
 local m = component.modem -- get primary modem component
 m.open(123)
 print(m.isOpen(123)) -- true
--- Send some message.
---m.broadcast(321, "this is a test")
--- Wait for a message from another network card.
-local _, _, from, port, _, message = event.pull("modem_message")
-print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
 
+while true do
+    local event, side, frequency, replyFrequency, message, distance = event.pull("modem_message")
+    print("Message received from the open modem on the "..side.." side of this computer.")
+    print("Frequency: "..frequency)
+    print("Requested reply frequency: "..replyFrequency)
+    print("Distance: "..distance)
+    print("Message is as follows: "..message)
+end
 
 
 
